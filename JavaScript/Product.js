50e103c13d251c51d01ecf77d25523d5371bbe10
Jack Steel <jacksteel@jacksteel.co.uk>
2016-12-19 18:53:21 +0000

    added type attribute to product class, started product loading

--- JavaScript/Product.js
+++ JavaScript/Product.js
@@ -1,4 +1,4 @@
-function Product(aName, aDescription, aPrice, aThumbnailPath, aBasePath, aObjFile, aMtlFile){
+function Product(aName, aDescription, aPrice, aThumbnailPath, aBasePath, aObjFile, aMtlFile, aType) {
 	this.name = aName;
 	this.description = aDescription;
 	this.basePrice = aPrice;
@@ -7,47 +7,49 @@ function Product(aName, aDescription, aPrice, aThumbnailPath, aBasePath, aObjFil
 	this.basePath = aBasePath;
 	this.objFile = aObjFile;
 	this.mtlFile = aMtlFile;
+	//0 = land, 1 = air, 2 = sea
+	this.type = aType
 	this.upgrades = [];
 }
-
 Product.prototype = {
-	constructor: Product,
-
-	getName:function(){
+	constructor: Product
+	, getName: function () {
 		return this.name;
-	},
-	getDescription:function(){
+	}
+	, getType: function () {
+		return this.type;
+	}
+	, getDescription: function () {
 		return this.description;
-	},
-	getPrice:function(){
+	}
+	, getPrice: function () {
 		return this.price;
-	},
-	getThumbnailPath:function(){
+	}
+	, getThumbnailPath: function () {
 		return this.thumbnailPath;
-	},
-	getBasePath:function(){
+	}
+	, getBasePath: function () {
 		return this.basePath;
-	},
-	getObjFile:function(){
+	}
+	, getObjFile: function () {
 		return this.objFile;
-	},
-	getMtlFile:function(){
+	}
+	, getMtlFile: function () {
 		return this.mtlFile;
-	},
-	getBasePrice:function(){
+	}
+	, getBasePrice: function () {
 		return this.basePrice;
-	},
-	setPrice:function(aPrice){
+	}
+	, setPrice: function (aPrice) {
 		this.price = aPrice;
-	},
-	calculateNewPrice:function(){
-		upgrades.forEach(function(element){
+	}
+	, calculateNewPrice: function () {
+		upgrades.forEach(function (element) {
 			this.price += element.getPrice();
 		})
-	},
-	addUpgrade:function(upgradeName, upgradePrice, upgradeValue){
-		var alteration = new Upgrade(upgradeName,upgradePrice,upgradeValue);
+	}
+	, addUpgrade: function (upgradeName, upgradePrice, upgradeValue) {
+		var alteration = new Upgrade(upgradeName, upgradePrice, upgradeValue);
 		this.upgrades.push(alteration);
-	},
-
-}
+	}
+, }
---
+++ JavaScript/ProductPage.js
@@ -0,0 +1,31 @@
+$(document).ready(function ($) {
+	console.log("ready");
+	var prodArr = init();
+
+});
+
+function init() {
+	var Products = [];
+	//aventador
+	Products.push(new Product("Lamborghini Aventador",
+							  "Forget the idea of performance you are used to. The Aventador Coupé has been engineered to revolutionize this concept and establish a new benchmark in the segment of super sports cars and beyond. This car aims to bring the future forward. This is a true supercar legend in the making, which combines the tradition of the Lamborghini brand with a level of innovation which takes the House of the Raging Bull to hitherto unexplored territory.",
+							  225000.00,
+							  "Images/thumbnails/Aventador.png",
+							  "Models/Aventador/",
+							  "Avent.obj",
+							  "Avent.mtl",
+							  0));
+	//blackhawk
+	Products.push(new Product("UH-60 Blackhawk",
+							  "The Sikorsky UH-60 Black Hawk is a four-bladed, twin-engine, medium-lift utility helicopter manufactured by Sikorsky Aircraft. Sikorsky submitted the S-70 design for the United States Army's Utility Tactical Transport Aircraft System (UTTAS) competition in 1972. The Army designated the prototype as the YUH-60A and selected the Black Hawk as the winner of the program in 1976, after a fly-off competition with the Boeing Vertol YUH-61.",
+							 20000000,
+							 "Imgages/thumbnails/Blackhawk.png",
+							 "Models/Blackhawk/",
+							 "uh60.obj",
+							 "uh60.mtl",
+							 1));
+
+
+
+	return Products;
+}
