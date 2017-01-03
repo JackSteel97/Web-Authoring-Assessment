e3368ec411f907c7df0c21fe3fe5eac68ea9d90e
Jack Steel <jacksteel@jacksteel.co.uk>
2017-01-03 13:24:58 +0000

    added customisation for aventador

--- JavaScript/ProductDetailsPage.js
+++ JavaScript/ProductDetailsPage.js
@@ -64,6 +64,13 @@ $(document).ready(function ($) {
 });
 
 function populateUpgradesForAventador() {
+	var specificHtml = '<li> <label for="armourUpgrade"><b>Armour</b></label> <div id="armourUpgrade" class="upgradeSection"> <input type="radio" name="armour" value="None" data-price="0" checked /> None&emsp; <input type="radio" name="armour" value="Light" data-price="15000" /> Light&emsp; <input type="radio" name="armour" value="Medium" data-price="25000" /> Medium&emsp; <input type="radio" name="armour" value="Heavy" data-price="50000" /> Heavy&emsp; </div> </li> <li> <label for="brakeUpgrade"><b>Brakes</b></label> <div id="brakeUpgrade" > <input type="radio" name="brakes" value="Stock" data-price="0" checked /> Stock&emsp; <input type="radio" name="brakes" value="Street" data-price="5000" /> Street&emsp; <input type="radio" name="brakes" value="Sport" data-price="7500" /> Sport&emsp; <input type="radio" name="brakes" value="Race" data-price="10000" /> Race&emsp; </div> </li> <li> <label for="engineUpgrade"><b>Brakes</b></label> <div id="engineUpgrade" > <input type="radio" name="engine" value="V6" data-price="0" checked /> V6&emsp; <input type="radio" name="engine" value="V8" data-price="14999.99" /> V8&emsp; <input type="radio" name="engine" value="V10" data-price="39999.99" /> V10&emsp; <input type="radio" name="engine" value="V12" data-price="79999.99" /> V12&emsp; </div> </li> <li> <label for="turboUpgrade"><b>Turbo</b></label> <div id="turboUpgrade"> <input type="checkbox" name="turbo" value="Turbo Charged" data-price="100000" /> Turbo Charged&emsp; </div> </li>'
+
+	//add the html for aventador
+	$("#configurator li").append(specificHtml);
+
+
+		//armour
 	$('#armourUpgrade input:radio').on('change', function () {
 		var value = $(this).val();
 		var cost = $(this).data("price");
@@ -76,6 +83,52 @@ function populateUpgradesForAventador() {
 			}));
 		}
 		applyUpgradeToProduct("Armour", cost, value);
+		updateTotalPrice();
+	});
+	//brakes
+	$('#brakeUpgrade input:radio').on('change', function () {
+		var value = $(this).val();
+		var cost = $(this).data("price");
+		if (value == "Stock") {
+			removeFromPartsList("brakes");
+		}
+		else {
+			addToPartsList("brakes", value + " Brakes", "£" + cost.toLocaleString('en-UK', {
+				minimumFractionDigits: 2
+			}));
+		}
+		applyUpgradeToProduct("Brakes", cost, value);
+		updateTotalPrice();
+	});
+	//engine
+	$('#engineUpgrade input:radio').on('change', function () {
+		var value = $(this).val();
+		var cost = $(this).data("price");
+		if (value == "V6") {
+			removeFromPartsList("engine");
+		}
+		else {
+			addToPartsList("engine", value + " Engine", "£" + cost.toLocaleString('en-UK', {
+				minimumFractionDigits: 2
+			}));
+		}
+		applyUpgradeToProduct("Engine", cost, value);
+		updateTotalPrice();
+	});
+	//turbo
+	$('#turboUpgrade input:checkbox').on('change', function () {
+		var value = $(this).val();
+		var cost = $(this).data("price");
+		if (!$(this).prop('checked')) {
+			removeFromPartsList("turbo");
+			applyUpgradeToProduct("Turbo", 0, "No");
+		}
+		else {
+			addToPartsList("turbo", value, "£" + cost.toLocaleString('en-UK', {
+				minimumFractionDigits: 2
+			}));
+			applyUpgradeToProduct("Turbo", cost, "Yes");
+		}
 		updateTotalPrice();
 	});
 }
