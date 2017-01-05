8f33ebee44010e9bbd408c5ca2a25f0c98ddd366
Jack Steel <jacksteel@jacksteel.co.uk>
2017-01-05 17:18:55 +0000

    added tank upgrades

--- JavaScript/ProductDetailsPage.js
+++ JavaScript/ProductDetailsPage.js
@@ -6,7 +6,6 @@ $(document).ready(function ($) {
 		window.location.href = "index.html";
 		return;
 	}
-
 	//get JSON object from storage and map it's attributes to a new product object so it can be used as needed.
 	product = $.extend(new Product(), JSON.parse(localStorage.getItem("currentProduct")));
 	console.log(product);
@@ -58,48 +57,42 @@ $(document).ready(function ($) {
 	$("#colourPicker").change(function (event) {
 		vehicle3DHandler.setPaintColour(this.value);
 	});
-
-	$(".cartBtn").click(function(event){
+	$(".cartBtn").click(function (event) {
 		product.thumbnailPath = $("#Container3D canvas")[0].toDataURL();
 		addProductToCart();
 	});
 	$("#loadingDisplay").hide();
 });
 
-function addProductToCart(){
+function addProductToCart() {
 	if (localStorage.getItem("cart") == null) {
 		var cartList = [];
 		cartList.push(product);
 		console.log(cartList);
-		localStorage.setItem("cart",JSON.stringify(cartList));
-
-	}else{
+		localStorage.setItem("cart", JSON.stringify(cartList));
+	}
+	else {
 		var cartList = JSON.parse(localStorage.getItem("cart"));
-		for(var i = 0; i<cartList.length; i++){
-			cartList[i] = $.extend(new Product(),cartList[i]);
+		for (var i = 0; i < cartList.length; i++) {
+			cartList[i] = $.extend(new Product(), cartList[i]);
 		}
 		cartList.push(product);
 		console.log(cartList);
-		localStorage.setItem("cart",JSON.stringify(cartList));
+		localStorage.setItem("cart", JSON.stringify(cartList));
 	}
-
-	localStorage.setItem("currentProduct",null);
+	localStorage.setItem("currentProduct", null);
 	moveToShoppingCart();
-
 }
 
-function moveToShoppingCart(){
+function moveToShoppingCart() {
 	window.location.href = "shoppingcart.html";
 }
 
 function populateUpgradesForAventador() {
 	var specificHtml = '<li> <label for="armourUpgrade"><b>Armour</b></label> <div id="armourUpgrade" class="upgradeSection"> <input type="radio" name="armour" value="None" data-price="0" checked /> None&emsp; <input type="radio" name="armour" value="Light" data-price="15000" /> Light&emsp; <input type="radio" name="armour" value="Medium" data-price="25000" /> Medium&emsp; <input type="radio" name="armour" value="Heavy" data-price="50000" /> Heavy&emsp; </div> </li> <li> <label for="brakeUpgrade"><b>Brakes</b></label> <div id="brakeUpgrade" > <input type="radio" name="brakes" value="Stock" data-price="0" checked /> Stock&emsp; <input type="radio" name="brakes" value="Street" data-price="5000" /> Street&emsp; <input type="radio" name="brakes" value="Sport" data-price="7500" /> Sport&emsp; <input type="radio" name="brakes" value="Race" data-price="10000" /> Race&emsp; </div> </li> <li> <label for="engineUpgrade"><b>Engine</b></label> <div id="engineUpgrade" > <input type="radio" name="engine" value="V6" data-price="0" checked /> V6&emsp; <input type="radio" name="engine" value="V8" data-price="14999.99" /> V8&emsp; <input type="radio" name="engine" value="V10" data-price="39999.99" /> V10&emsp; <input type="radio" name="engine" value="V12" data-price="79999.99" /> V12&emsp; </div> </li> <li> <label for="turboUpgrade"><b>Turbo</b></label> <div id="turboUpgrade"> <input type="checkbox" name="turbo" value="Turbo Charged" data-price="100000" /> Turbo Charged&emsp; </div> </li>'
-
-	//add the html for aventador
-	$("#configurator li").append(specificHtml);
-
-
-		//armour
+		//add the html for aventador
+	$("#configurator").append(specificHtml);
+	//armour
 	$('#armourUpgrade input:radio').on('change', function () {
 		var value = $(this).val();
 		var cost = $(this).data("price");
@@ -167,7 +160,9 @@ function populateUpgradesForBlackhawk() {}
 function populateUpgradesForBoeing787() {}
 
 function populateUpgradesForAbrams() {
-
+	var specificHTML = '<li> <label for="enginePower"><b>Engine Power:</b></label> <div id="enginePower"> <input type="range" min="1500" max="2500" value="1500" data-price="5000"/> <span id="val">1500 hp</span> </div> </li> <li> <label for="reactiveArmour"><b>Turbo</b></label> <div id="reactiveArmour"> <input type="checkbox" name="reactiveArmour" value="Reactive Armour Plates" data-price="3000000" /> Reactive Armour Plates&emsp; </div> </li> <li> <label for="missileCountermeasures"><b>Turbo</b></label> <div id="missileCountermeasures"> <input type="checkbox" name="missileCountermeasures" value="Missile Countermeasures" data-price="1500000" /> Missile Countermeasures&emsp; </div> </li> <li> <label for="depletedUrainiumArmour"><b>Turbo</b></label> <div id="depletedUrainiumArmour"> <input type="checkbox" name="depletedUrainiumArmour" value="Depleted Urainium Armour" data-price="750000" /> Depleted Urainium Armour&emsp; </div> </li> ';
+		//add the html for abrams tank
+	$("#configurator").append(specificHTML);
 	//engine
 	$('#enginePower input').on('input', function () {
 		var value = $(this).val();
@@ -184,20 +179,51 @@ function populateUpgradesForAbrams() {
 		applyUpgradeToProduct("Engine Power", cost, value + "hp");
 		updateTotalPrice();
 	});
-
 	//reactiveArmour
-	$('#turboUpgrade input:checkbox').on('change', function () {
+	$('#reactiveArmour input:checkbox').on('change', function () {
 		var value = $(this).val();
 		var cost = $(this).data("price");
 		if (!$(this).prop('checked')) {
 			removeFromPartsList("reactiveArmour");
-			applyUpgradeToProduct("Reactive Armour", 0, "No");
+			removeUpgradeFromProduct("Reactive Armour Plates")
 		}
 		else {
-			addToPartsList("turbo", value, "£" + cost.toLocaleString('en-UK', {
+			addToPartsList("reactiveArmour", value, "£" + cost.toLocaleString('en-UK', {
 				minimumFractionDigits: 2
 			}));
-			applyUpgradeToProduct("Turbo", cost, "Yes");
+			applyUpgradeToProduct("Reactive Armour Plates", cost, "Yes");
+		}
+		updateTotalPrice();
+	});
+	//missileCountermeasures
+	$('#missileCountermeasures input:checkbox').on('change', function () {
+		var value = $(this).val();
+		var cost = $(this).data("price");
+		if (!$(this).prop('checked')) {
+			removeFromPartsList("missileCountermeasures");
+			removeUpgradeFromProduct("Missile Countermeasures")
+		}
+		else {
+			addToPartsList("missileCountermeasures", value, "£" + cost.toLocaleString('en-UK', {
+				minimumFractionDigits: 2
+			}));
+			applyUpgradeToProduct("Missile Countermeasures", cost, "Yes");
+		}
+		updateTotalPrice();
+	});
+	//depletedUrainiumArmour
+	$('#depletedUrainiumArmour input:checkbox').on('change', function () {
+		var value = $(this).val();
+		var cost = $(this).data("price");
+		if (!$(this).prop('checked')) {
+			removeFromPartsList("depletedUrainiumArmour");
+			removeUpgradeFromProduct("Depleted Urainium Armour")
+		}
+		else {
+			addToPartsList("depletedUrainiumArmour", value, "£" + cost.toLocaleString('en-UK', {
+				minimumFractionDigits: 2
+			}));
+			applyUpgradeToProduct("Depleted Urainium Armour", cost, "Yes");
 		}
 		updateTotalPrice();
 	});
@@ -232,7 +258,7 @@ function applyUpgradeToProduct(name, price, value) {
 	product.calculateNewPrice();
 }
 
-function removeUpgradeFromProduct(name){
+function removeUpgradeFromProduct(name) {
 	product.removeUpgrade(name);
 	product.calculateNewPrice();
 }
